84096f3d8062ec06e9aac2bf3b81d7937df224b1
rojleena <rojleenapegu05@gmail.com>
2026-03-06 09:20:14 +0000

    added the code and error checking for s-type instructions

--- assembler.py
+++ assembler.py
@@ -125,20 +125,21 @@ def encode_itype(name, operands, instructions, registers):
 
 if __name__ == "__main__":
 
-    print("ErrorGen Tests")
+    print("\nErrorGen Tests of I-type")
     print(encode_itype("addi", ["s1","s2"], instructions, registers)) #testing for wrong operand count
     print(encode_itype("sltiu", ["x99", "s2", "10"], instructions, registers)) #testing for invalid registor name
 
-    print("\n SimpleBin Tests")
+    print("\nSimpleBin Tests")
     r1 = encode_itype("sltiu", ["s1", "s2", "10"], instructions, registers) #checking arithmetic syntax
 print("sltiu s1, s2, 10 =", r1)
 
 r2 = encode_itype("jalr", ["ra", "0", "zero"], instructions, registers) #checking load-style syntax
 print("jalr ra, 0(zero) =", r2)
 
-print("\n HardGen Test")
+print("\nHardGen Test")
 r3 = encode_itype("addi", ["t0", "t1", "-2048"], instructions, registers) # checks for maximum negative 12 bit value
 print("addi t0, t1, -2048 =", r3)
+
 
 #next is U- type instructions
 def encode_utype(instruction, rd, imm):
@@ -160,7 +161,61 @@ print ("lui s1, 100 =", encode_utype('lui', 's1', 100))
 
 
 
+# code for the s-type instruction
 
+def encode_stype(name, operands, instructions, registers):
+	
+    #checking if the instruction name is present in instruction mapping
+    if name not in instructions:
+        return f"Error: unknown instruction '{name}'"
+
+    info = instructions[name]
+
+    #s-type require 3 operands
+    if len(operands) != 3:		
+        return f"Errror: '{name}' needs 3 operands "
+
+    try:
+        rs2 = registers[operands[0]]
+        imm_val = int(operands[1])
+        rs1 = registers[operands[2]]
+
+        if imm_val < -2048 or imm_val > 2047:
+            return "Error: immediate out of range"
+
+	# convert immediate to binary
+        imm = format(imm_val & 0xFFF, '012b')
+
+        # split immediate for S-type format
+        imm_high = imm[:7]   # imm[11:5]
+        imm_low = imm[7:]    # imm[4:0]
+
+
+        binarycode = imm_high + rs2 + rs1 + info["f3"] + imm_low + info["opcode"]
+
+        return binarycode
+
+    except KeyError:
+        return "Error: invalid register"
+    except ValueError:
+        return "Error: invalid immediate value"
+
+if __name__ == "__main__":
+
+    print("\nErrorGen Tests of S-type instruction")
+    print(encode_stype("sw", ["s1", "8"], instructions, registers))
+    print(encode_stype("sw", ["x99", "8", "s2"], instructions, registers))
+
+    print("\nSimpleBin Tests")
+    r1 = encode_stype("sw", ["s1", "8", "s2"], instructions, registers)
+print("sw s1, 8(s2) =", r1)
+
+r2 = encode_stype("sw", ["t0", "16", "sp"], instructions, registers)
+print("sw t0, 16(sp) =", r2)
+
+print("\nHardGen Test")
+r3 = encode_stype("sw", ["t1", "-2048", "sp"], instructions, registers)
+print("sw t1, -2048(sp) =", r3)
 
 
 
